77dc9a6176f527aeeea88089e01d6febd3885dc0
Maxhd4321 <hd2943240@gmail.com>
2025-07-13 16:31:33 +0200

    updated files to current 1.6 version

---
+++ Source/MD_Go_To_Sleep/GoToSleepMod.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace KB_Go_To_Sleep
+{
+    public class GoToSleepMod : Mod
+    {
+        private static GoToSleepMod _instance;
+        public static GoToSleepMod Instance => _instance;
+
+        public GoToSleepMod(ModContentPack content) : base(content)
+        {
+            _instance = this;
+        }
+    }
+}
